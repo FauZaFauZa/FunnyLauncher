d0e75a0adfb6b7c8e5f951fea560d820b20102e7
FauZaFauZa <fauza.officel@gmail.com>
2021-10-21 21:29:58 +0200

    Grosse maj sa daronne la pute

--- app/assets/js/scripts/landing.js
+++ app/assets/js/scripts/landing.js
@@ -130,7 +130,7 @@ function updateSelectedAccount(authUser){
             username = authUser.displayName
         }
         if(authUser.uuid != null){
-            document.getElementById('avatarContainer').style.backgroundImage = `url('https://mc-heads.net/body/${authUser.uuid}/right')`
+            document.getElementById('avatarContainer').style.backgroundImage = `url('https://crafatar.com/renders/head/$%7Baccounts[i].uuid%7D?scale=2&default=MHF_Steve&overlay')`
         }
     }
     user_text.innerHTML = username
